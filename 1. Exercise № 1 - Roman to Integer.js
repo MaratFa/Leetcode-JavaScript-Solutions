ede128c587c1879f1c2ec89e696a6df3e7cc319e
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-16 18:37:44 +0300

    Update 1. Exercise № 1 - Roman to Integer.js

--- 1. Exercise № 1 - Roman to Integer.js
+++ 1. Exercise № 1 - Roman to Integer.js
@@ -44,7 +44,7 @@ symbols = {
     "M": 1000
 };
 
-var romanToInt = function(s) {
+let romanToInt = function(s) {
     value = 0;
     for(let i = 0; i < s.length; i++){
         symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
@@ -55,7 +55,7 @@ var romanToInt = function(s) {
 
 /*  -- Solution 2 --  */
 
-var romanToInt = function(s) {
+let romanToInt = function(s) {
     const map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
     let num = 0;
     
